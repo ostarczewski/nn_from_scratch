1ce5b7745ac33f33fd967b457c9fa42645bba02c
ostarczewski <ostarczewski@student.agh.edu.pl>
2025-02-26 17:58:22 +0100

    matrix multi logic, He initialization, 0.01 bias for relu

--- layer.py
+++ layer.py
@@ -16,27 +16,17 @@ class Layer:
 
 class Dense(Layer):
     def __init__(self, input_size, output_size):
-        # tutaj kolejność zmienia to czy 1 neuron to kolumna czy rząd
-        # możliwe, że będzie błąd w mnożeniu przez różne wielkości macierzy jak się to na odwrót zrobi
-        # self.weights = np.random.randn(input_size, output_size) 
-        self.weights = np.random.randn(output_size, input_size)
+        # self.weights = np.random.randn(input_size, output_size)                               # random initialization
+        self.weights = np.random.normal(0, np.sqrt(2/input_size), (input_size, output_size))  # He initialization
+        # N ~ zero avg, sqrt(2/input_size) std
         self.bias = np.zeros(output_size)
-        # self.bias = np.random.randn(output_size)
+        # self.bias = np.full(output_size, 0.01)  # można dać 0.01 dla ReLU
 
-        # considerations:
-        # Small weights pevent exploding/vanishing gradients
-        # Bias is initialized to zero because it doesn’t need to be random—it shifts the output, and training will adjust it.
 
     def forward(self, input):
-        self.input = input
-        self.output = np.dot(input, self.weights) + self.bias
+        self.input = input  # zapisywanie inputu, po co? do back prop?
+        self.output = self.input @ self.weights + self.bias
+        # [1,input] @ [input,output] + [1,output]  =>  [1,output]
 
     def backward(self, output_grad, learning_rate):
-        # olala
-        # raise NotImplementedError("Not implemented yet")
-        # tutaj znowu kolejność ma znaczenie
-        # weights_grad = np.dot(self.input.T, output_grad)   ???
-        weights_grad = np.dot(output_grad, self.input.T) 
-
-
-        # ja muszę sobie to wszystko zrozumieć na kartce, a potem mogę to zaimplementować
+        raise NotImplementedError("Not implemented yet")
